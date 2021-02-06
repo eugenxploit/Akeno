34e16700af90124424239c6324200333aa3d3944
Euegen <74448399+eugenxploit@users.noreply.github.com>
2021-02-06 06:14:40 -0800

    Update __main__.py

--- Elizabeth/__main__.py
+++ Elizabeth/__main__.py
@@ -64,7 +64,8 @@ buttons += [[InlineKeyboardButton(text="🔒 CLOSE THE MENU 🔒",
 
 HELP_STRINGS = f"""
 [Nisshoku 😼](https://telegra.ph/file/e323dbcb71201eb41f727.jpg) comes with handy tools which makes chat management easier,
-Click on the buttons below to get usage instructions ^_^"""
+Click on the buttons below to get usage instructions ^_^
+"""
 
 
 IMPORTED = {}
