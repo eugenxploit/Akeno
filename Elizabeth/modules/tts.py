0b6b4be6b6b2221787acf79dd11ab6e312c55335
Euegen <74448399+eugenxploit@users.noreply.github.com>
2021-02-08 09:51:46 -0800

    Update and rename Text_convert_to_speech.py to tts.py

--- Elizabeth/modules/tts.py
+++ Elizabeth/modules/tts.py
@@ -82,3 +82,21 @@ async def _(event):
                              voice_note=True,
                              reply_to=reply_to_id)
         os.remove("k.mp3")
+        
+__mod_name__ = "Speech"
+
+__help__ =  """
+*TEXT TO SPEECH / SPEECH TO TEXT*
+ 
+• /tts <lang | text>: Returns a speech note of the text provided.
+• /stt: Reply to a voice message(English only) to extract text from it.
+
+⚙️ *Language codes:
+
+`af`,`am`,`ar`,`az`,`be`,`bg`,`bn`,`bs`,`ca`,`ceb`,`co`,`cs`,`cy`,`da`,`de`,`el`,`en`,`eo`,`es`,
+`et`,`eu`,`fa`,`fi`,`fr`,`fy`,`ga`,`gd`,`gl`,`gu`,`ha`,`haw`,`hi`,`hmn`,`hr`,`ht`,`hu`,`hy`,
+`id`,`ig`,`is`,`it`,`iw`,`ja`,`jw`,`ka`,`kk`,`km`,`kn`,`ko`,`ku`,`ky`,`la`,`lb`,`lo`,`lt`,`lv`,`mg`,`mi`,`mk`,
+`ml`,`mn`,`mr`,`ms`,`mt`,`my`,`ne`,`nl`,`no`,`ny`,`pa`,`pl`,`ps`,`pt`,`ro`,`ru`,`sd`,`si`,`sk`,`sl`,
+`sm`,`sn`,`so`,`sq`,`sr`,`st`,`su`,`sv`,`sw`,`ta`,`te`,`tg`,`th`,`tl`,`tr`,`uk`,`ur`,`uz``
+`vi`,`xh`,`yi`,`yo`,`zh`,`zh_CN`,`zh_TW`,`zu`
+"""
