2e261bb5c0b84b74c8a52253e811278af0a9c998
Euegen <74448399+eugenxploit@users.noreply.github.com>
2021-02-10 01:41:22 -0800

    Update regex.py

--- Elizabeth/modules/regex.py
+++ Elizabeth/modules/regex.py
@@ -131,16 +131,14 @@ SED_HANDLER = DisableAbleMessageHandler(Filters.regex(
 dispatcher.add_handler(SED_HANDLER)
 
 __mod_name__ = "Regex / Sed"
-__help__ = """ 
- • s/<text1>/<text2>(/<flag>): Reply to a message with this to perform a sed operation on that message, 
-replacing all occurrences of 'text1' with 'text2'. Flags are optional, 
-and currently include 'i' for ignore case, 'g' for global, or nothing. 
-Delimiters include /, _, |, and :. 
- • Text grouping is supported. 
- • The resulting message cannot be larger than 4096.
 
-*Reminder*: Sed uses some special characters to make matching easier, such as these: +*.?\
+
+__help__ = """
+ - s/<text1>/<text2>(/<flag>): Reply to a message with this to perform a sed operation on that message, replacing all \
+occurrences of 'text1' with 'text2'. Flags are optional, and currently include 'i' for ignore case, 'g' for global, \
+or nothing. Delimiters include `/`, `_`, `|`, and `:`. Text grouping is supported. The resulting message cannot be \
+larger than {}.
+*Reminder:* Sed uses some special characters to make matching easier, such as these: `+*.?\\`
 If you want to use these characters, make sure you escape them!
-Example: \?.
+eg: \\?.
 """.format(telegram.MAX_MESSAGE_LENGTH)
-
