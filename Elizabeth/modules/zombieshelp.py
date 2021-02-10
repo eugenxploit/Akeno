7d46e967e2032b61ace18e43ad29eda04cabc71d
Euegen <74448399+eugenxploit@users.noreply.github.com>
2021-02-10 05:18:22 -0800

    Delete zombieshelp.py

--- Elizabeth/modules/zombieshelp.py
+++
@@ -1,8 +0,0 @@
-__help__ """
-⚙️ *Zombies (Clears deleted accounts from group)
-
- • /zombies: counts the number of deleted account in your group.
- • /zombies clean: Removes deleted accounts from group.
-""" 
-
-__mod_name__ = "Zombies"
