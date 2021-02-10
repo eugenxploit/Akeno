ee0be50dbf446d08de469883bab8712e528a66c5
Euegen <74448399+eugenxploit@users.noreply.github.com>
2021-02-10 04:05:16 -0800

    Update bans.py

--- Elizabeth/modules/bans.py
+++ Elizabeth/modules/bans.py
@@ -397,12 +397,14 @@ Some people need to be publicly banned; spammers, annoyances, or just trolls.
 This module allows you to do that easily, by exposing some common actions, so everyone will see!
  • /kickme: Kicks the user who issued the command
  • /banme: Bans the user who issued the command
-*Admin only:*
+
+⚙️ *Admin only:*
  • /ban <userhandle>: Bans a user. (via handle, or reply)
  • /tban <userhandle> x(m/h/d): Bans a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
  • /unban <userhandle>: Unbans a user. (via handle, or reply)
  • /kick <userhandle>: Kicks a user, (via handle, or reply)
-An example of temporarily banning someone:
+
+⚙️ *An example of temporarily banning someone:*
 `/tban @username 2h`; this bans a user for 2 hours.
 """
 
