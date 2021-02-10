833bbd4676682091157ff6a0d552ba968ab6b5ec
Euegen <74448399+eugenxploit@users.noreply.github.com>
2021-02-10 05:11:53 -0800

    Update zombies.py

--- Elizabeth/modules/zombies.py
+++ Elizabeth/modules/zombies.py
@@ -113,11 +113,4 @@ async def zombies(event):
 
     await cleaning_zombies.edit(del_status)
 
-__help__ """
-⚙️ *Zombies (Clears deleted accounts from group)
 
-• /zombies: counts the number of deleted account in your group.
-• /zombies clean: Removes deleted accounts from group..
-""" 
-
-__mod_name__ = "Zombies"
