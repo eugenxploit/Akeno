d3630b01ab3ef4a30be4ddb7f1378344d5f2accb
Euegen <74448399+eugenxploit@users.noreply.github.com>
2021-02-11 00:42:59 -0800

    Update __init__.py

--- Elizabeth/__init__.py
+++ Elizabeth/__init__.py
@@ -3,7 +3,7 @@ import time
 import os
 import sys
 import spamwatch
-from pyrogram import Client, errors
+#from pyrogram import Client, errors
 from telethon import TelegramClient
 import telegram.ext as tg
 
@@ -205,7 +205,7 @@ else:
 api_id = TELETHON_ID
 api_hash = TELETHON_HASH
 client = TelegramClient("Elizabeth", api_id, api_hash)
-nyaa = Client("Elizabeth", api_id=TELETHON_ID, api_hash=TELETHON_HASH, bot_token=TOKEN)
+#nyaa = Client("Elizabeth", api_id=TELETHON_ID, api_hash=TELETHON_HASH, bot_token=TOKEN)
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 
