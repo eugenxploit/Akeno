30b5d206fbafac549e593634ffc7e87e4312fbf9
Euegen <74448399+eugenxploit@users.noreply.github.com>
2021-02-28 02:05:25 -0800

    Update strings.py

--- Elizabeth/modules/tr_engine/strings.py
+++ Elizabeth/modules/tr_engine/strings.py
@@ -21,7 +21,7 @@ from codecs import encode, decode
 from Elizabeth import LOGGER
 from Elizabeth.modules.sql.locales_sql import prev_locale
 
-LANGUAGES = ['en-US', 'en-GB', 'id', 'ru', 'es']
+LANGUAGES = ['en-US']
 
 strings = {}
 
@@ -39,26 +39,7 @@ def tld(chat_id, t, show_none=True):
                 encode(strings['en-US'][t], 'latin-1', 'backslashreplace'),
                 'unicode-escape')
             return result
-        elif LOCALE in ('en-GB') and t in strings['en-GB']:
-            result = decode(
-                encode(strings['en-GB'][t], 'latin-1', 'backslashreplace'),
-                'unicode-escape')
-            return result
-        elif LOCALE in ('id') and t in strings['id']:
-            result = decode(
-                encode(strings['id'][t], 'latin-1', 'backslashreplace'),
-                'unicode-escape')
-            return result
-        elif LOCALE in ('ru') and t in strings['ru']:
-            result = decode(
-                encode(strings['ru'][t], 'latin-1', 'backslashreplace'),
-                'unicode-escape')
-            return result
-        elif LOCALE in ('es') and t in strings['es']:
-            result = decode(
-                encode(strings['es'][t], 'latin-1', 'backslashreplace'),
-                'unicode-escape')
-            return result
+      
 
     if t in strings['en-US']:
         result = decode(
@@ -78,14 +59,6 @@ def tld_list(chat_id, t):
         LOCALE = LANGUAGE.locale_name
         if LOCALE in ('en-US') and t in strings['en-US']:
             return strings['en-US'][t]
-        elif LOCALE in ('en-GB') and t in strings['en-GB']:
-            return strings['en-GB'][t]
-        elif LOCALE in ('id') and t in strings['id']:
-            return strings['id'][t]
-        elif LOCALE in ('ru') and t in strings['ru']:
-            return strings['ru'][t]
-        elif LOCALE in ('es') and t in strings['es']:
-            return strings['es'][t]
 
     if t in strings['en-US']:
         return strings['en-US'][t]
