b8cd381c33479191b6e0d2eabc529703ea24eddb
Euegen <74448399+eugenxploit@users.noreply.github.com>
2021-02-28 16:12:18 -0800

    Create fun.py

---
+++ Elizabeth/modules/fun.py
@@ -0,0 +1,34 @@
+import html
+import random
+import time
+
+from telegram import ParseMode, Update, ChatPermissions
+from telegram.ext import CallbackContext, run_async, Filters, MessageHandler
+from telegram.error import BadRequest
+
+import AkenoHimejimabot.modules.fun_strings as fun_strings
+from AkenoHimejimabot import dispatcher
+from AkenoHimejimabot.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
+from AkenoHimejimabot.modules.helper_funcs.chat_status import (is_user_admin)
+from AkenoHimejimabot.modules.helper_funcs.extraction import extract_user
+
+ARAGIFS = [
+         'CgACAgUAAx0CTCs8lgABAg1KX6e6iLkD3UVmuqDXJKWg8C1dljwAArgBAAK0pThVvwOSjYhlMsQeBA',
+         'CgACAgUAAx0CTCs8lgABAg8eX6fCo-C_5jepnqtlEljgbNuXbnsAAtABAAL8LUFVgFSPB8jG874eBA',
+          'CgACAgUAAx0CTCs8lgABAhAXX6fIxIEa1yx5BrOkmuqggwAB0ePMAAL-AQACCXk5Vc8MzDnCRP7xHgQ'
+]
+
+
+@run_async
+def ara(update: Update, context: CallbackContext):
+    message = update.effective_message
+    name = message.reply_to_message.from_user.first_name if message.reply_to_message else message.from_user.first_name
+    reply_animation = message.reply_to_message.reply_animation if message.reply_to_message else message.reply_animation
+    reply_animation(
+        random.choice(fun_strings.ARAGIFS), caption=f'Ara Ara! {name}')
+    
+    
+    
+    
+ARA_HANDLER = DisableAbleCommandHandler("ara", ara)
+ARA_REGEX_HANDLER = DisableAbleMessageHandler(Filters.regex(r"^Ara(.*)$"), ara, friendly="ara")
